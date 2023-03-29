2671b65590d65d661ad830dbd0df1d3299fc9360
Alousius <alousiusambrose18@gmail.com>
2023-03-29 12:48:57 +0530

    video added

--- scripts.js
+++ scripts.js
@@ -75,4 +75,22 @@ counters.forEach((counter) => {
   updateCounter();
 });
 
+function show() {
+  var reveal = document.querySelectorAll(".reveal");
+
+  for (var i = 1; i < reveal.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal[i].getBoundingClientRect().top;
+    var e = 190;
+
+    if (elementTop < windowHeight-e) {
+      reveal[i].classList.add("active");}
+    else {
+      reveal[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", show);
+
 
